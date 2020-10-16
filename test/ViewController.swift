d9ece7b36fae258e754ff56d564c23af43d04627
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2020-10-16 17:05:01 +0300

    Test

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -385,3 +385,302 @@ extension TestVC: UIContextMenuInteractionDelegate {
         return configuration
     }
 }
+
+class ChatTextView: UIView {
+    
+    private let placeholderLabel = UILabel()
+    
+    let textView = UITextView()
+    
+    private var heightConstraint: NSLayoutConstraint?
+    
+    var text: String? {
+        get {
+            return textView.text
+        }
+        set {
+            textView.text = newValue
+            applyHeight()
+            applyPlaceholderLabel()
+        }
+    }
+    
+    var placeholder: String? {
+        get {
+            return placeholderLabel.text
+        }
+        set {
+            placeholderLabel.text = newValue
+        }
+    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    // MARK: - Private
+    
+    private func setup() {
+        setupTextView()
+        setupPlaceholderLabel()
+        text = nil
+        backgroundColor = .clear
+        NotificationCenter.default.addObserver(self, selector: #selector(UITextInputDelegate.textDidChange(_:)), name: UITextView.textDidChangeNotification, object: textView)
+        placeholder = "Long placeholder text, very long! I'm not joking!"
+    }
+    
+    private func setupTextView() {
+        textView.backgroundColor = .clear
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.systemFont(ofSize: 15.0)
+//        textView.contentInset = .zero
+//        textView.textContainerInset = .zero
+        addSubview(textView)
+        addConstraint(NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: textView, attribute: .left, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: textView, attribute: .right, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: textView, attribute: .top, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: textView, attribute: .bottom, multiplier: 1.0, constant: 0.0))
+        heightConstraint = NSLayoutConstraint.init(item: textView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 32.0)
+        textView.addConstraint(heightConstraint!)
+    }
+    
+    private func setupPlaceholderLabel() {
+        placeholderLabel.numberOfLines = 1
+        placeholderLabel.backgroundColor = .clear
+        placeholderLabel.lineBreakMode = .byTruncatingTail
+        placeholderLabel.font = UIFont.systemFont(ofSize: 15.0)
+        placeholderLabel.textColor = UIColor(red: 0.38, green: 0.38, blue: 0.38, alpha: 1.0)
+        placeholderLabel.isUserInteractionEnabled = false
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(placeholderLabel)
+        addConstraint(NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: placeholderLabel, attribute: .left, multiplier: 1.0, constant: -6.0))
+        addConstraint(NSLayoutConstraint(item: self, attribute: .right, relatedBy: .greaterThanOrEqual, toItem: placeholderLabel, attribute: .right, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: placeholderLabel, attribute: .centerY, multiplier: 1.0, constant: 0.0))
+    }
+    
+    private func applyPlaceholderLabel() {
+        let text = self.text ?? ""
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+    
+    private func applyHeight() {
+        textChanged()
+        let label = UILabel()
+        label.font = textView.font
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = text
+        label.sizeToFit()
+        let size = textView.sizeThatFits(CGSize.init(width: textView.frame.width/* - textView.textContainerInset.top - textView.textContainerInset.bottom*/, height: .greatestFiniteMagnitude))
+        heightConstraint?.constant = size.height < 32.0 ? 32.0 : (size.height/* + textView.textContainerInset.top + textView.textContainerInset.bottom*/)
+    }
+    
+    func textChanged() {
+        let caretRect = textView.caretRect(for: textView.selectedTextRange!.start)
+        let overflow = caretRect.size.height + caretRect.origin.y - (textView.contentOffset.y + textView.bounds.size.height - textView.contentInset.bottom - textView.contentInset.top)
+        if overflow > 0 {
+            //Fix wrong offset when cursor jumps to next line un explisitly
+            let seekEndY = textView.contentSize.height - textView.bounds.size.height
+            if textView.contentOffset.y != seekEndY {
+                textView.contentOffset.y = seekEndY
+            }
+        }
+    }
+    
+    // MARK: - objc
+    
+    @objc private func textDidChange(_ note: Notification) {
+        applyHeight()
+        applyPlaceholderLabel()
+    }
+}
+
+@objc public protocol GrowingTextViewDelegate: UITextViewDelegate {
+    @objc optional func textViewDidChangeHeight(_ textView: GrowingTextView, height: CGFloat)
+}
+
+@IBDesignable @objc
+open class GrowingTextView: UITextView {
+    override open var text: String! {
+        didSet { setNeedsDisplay() }
+    }
+    private var heightConstraint: NSLayoutConstraint?
+    
+    // Maximum length of text. 0 means no limit.
+    @IBInspectable open var maxLength: Int = 0
+    
+    // Trim white space and newline characters when end editing. Default is true
+    @IBInspectable open var trimWhiteSpaceWhenEndEditing: Bool = true
+    
+    // Customization
+    @IBInspectable open var minHeight: CGFloat = 0 {
+        didSet { forceLayoutSubviews() }
+    }
+    @IBInspectable open var maxHeight: CGFloat = 0 {
+        didSet { forceLayoutSubviews() }
+    }
+    @IBInspectable open var placeholder: String? {
+        didSet { setNeedsDisplay() }
+    }
+    @IBInspectable open var placeholderColor: UIColor = UIColor(white: 0.8, alpha: 1.0) {
+        didSet { setNeedsDisplay() }
+    }
+    @IBInspectable open var attributedPlaceholder: NSAttributedString? {
+        didSet { setNeedsDisplay() }
+    }
+    
+    // Initialize
+    override public init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        commonInit()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        contentMode = .redraw
+        associateConstraints()
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: self)
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidEndEditing), name: UITextView.textDidEndEditingNotification, object: self)
+        placeholder = "Long placeholder text, very long! I'm not joking!"
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    open override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 30)
+    }
+    
+    private func associateConstraints() {
+        // iterate through all text view's constraints and identify
+        // height,from: https://github.com/legranddamien/MBAutoGrowingTextView
+        for constraint in constraints {
+            if (constraint.firstAttribute == .height) {
+                if (constraint.relation == .equal) {
+                    heightConstraint = constraint;
+                }
+            }
+        }
+    }
+    
+    // Calculate and adjust textview's height
+    private var oldText: String = ""
+    private var oldSize: CGSize = .zero
+    
+    private func forceLayoutSubviews() {
+        oldSize = .zero
+        setNeedsLayout()
+        layoutIfNeeded()
+    }
+    
+    private var shouldScrollAfterHeightChanged = false
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if text == oldText && bounds.size == oldSize { return }
+        oldText = text
+        oldSize = bounds.size
+        
+        let size = sizeThatFits(CGSize(width: bounds.size.width, height: CGFloat.greatestFiniteMagnitude))
+        var height = size.height
+        
+        // Constrain minimum height
+        height = minHeight > 0 ? max(height, minHeight) : height
+        
+        // Constrain maximum height
+        height = maxHeight > 0 ? min(height, maxHeight) : height
+        
+        // Add height constraint if it is not found
+        if (heightConstraint == nil) {
+            heightConstraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: height)
+            addConstraint(heightConstraint!)
+        }
+        
+        // Update height constraint if needed
+        if height != heightConstraint!.constant {
+            shouldScrollAfterHeightChanged = true
+            heightConstraint!.constant = height
+            if let delegate = delegate as? GrowingTextViewDelegate {
+                delegate.textViewDidChangeHeight?(self, height: height)
+            }
+        } else if shouldScrollAfterHeightChanged {
+            shouldScrollAfterHeightChanged = false
+            scrollToCorrectPosition()
+        }
+    }
+    
+    private func scrollToCorrectPosition() {
+        if self.isFirstResponder {
+            self.scrollRangeToVisible(NSMakeRange(-1, 0)) // Scroll to bottom
+        } else {
+            self.scrollRangeToVisible(NSMakeRange(0, 0)) // Scroll to top
+        }
+    }
+    
+    // Show placeholder if needed
+    override open func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        if text.isEmpty {
+            let xValue = textContainerInset.left + textContainer.lineFragmentPadding
+            let yValue = textContainerInset.top
+            let width = rect.size.width - xValue - textContainerInset.right
+            let height = rect.size.height - yValue - textContainerInset.bottom
+            let placeholderRect = CGRect(x: xValue, y: yValue, width: width, height: height)
+            
+            if let attributedPlaceholder = attributedPlaceholder {
+                // Prefer to use attributedPlaceholder
+                attributedPlaceholder.draw(in: placeholderRect)
+            } else if let placeholder = placeholder {
+                // Otherwise user placeholder and inherit `text` attributes
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = textAlignment
+                var attributes: [NSAttributedString.Key: Any] = [
+                    .foregroundColor: placeholderColor,
+                    .paragraphStyle: paragraphStyle
+                ]
+                if let font = font {
+                    attributes[.font] = font
+                }
+                
+                placeholder.draw(in: placeholderRect, withAttributes: attributes)
+            }
+        }
+    }
+    
+    // Trim white space and new line characters when end editing.
+    @objc func textDidEndEditing(notification: Notification) {
+        if let sender = notification.object as? GrowingTextView, sender == self {
+            if trimWhiteSpaceWhenEndEditing {
+                text = text?.trimmingCharacters(in: .whitespacesAndNewlines)
+                setNeedsDisplay()
+            }
+            scrollToCorrectPosition()
+        }
+    }
+    
+    // Limit the length of text
+    @objc func textDidChange(notification: Notification) {
+        if let sender = notification.object as? GrowingTextView, sender == self {
+            if maxLength > 0 && text.count > maxLength {
+                let endIndex = text.index(text.startIndex, offsetBy: maxLength)
+                text = String(text[..<endIndex])
+                undoManager?.removeAllActions()
+            }
+            setNeedsDisplay()
+        }
+    }
+}
