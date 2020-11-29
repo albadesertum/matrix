2c3f36bf215a1ced41325c96a998ba9d597952f2
Vladimir Psyukalov <albadesertum@gmail.com>
2020-11-29 19:10:31 +0300

    Add observer

---
+++ matrix/Source/Observable/WeakReference.swift
@@ -0,0 +1,14 @@
+//
+//  WeakReference.swift
+//  WeakReference
+//
+//  Created by Vladimir Psyukalov on 01.01.2020.
+//  Copyright © 2020 Vladimir Psyukalov. All rights reserved.
+//
+
+import Foundation
+
+public struct WeakReference {
+    
+    public weak var reference: AnyObject?
+}
